9cde27b0f2caf3a36fe49b8e1b7b0b4fbdacbbd5
Akane <akane1033@gmail.com>
2023-11-21 11:02:15 +0100

    JS POST task request

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // GET tasks
   $.ajax({
     type: 'GET',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1090',
     dataType: 'json',
     success: function(response, textStatus) {
       response.tasks.forEach(function(task) {
@@ -13,6 +13,33 @@ $(document).ready(function() {
       console.log(errorMessage);
     }
   });
+
+  // POST tasks
+  var addTask = function() {
+    var newTask = document.getElementById('newTask').value;
+    if (newTask) {
+      $.ajax({
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1090',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          task: {
+            content: newTask
+          }
+        }),
+        success: function(response, textStatus) {
+          $('#list').append('<p>' + response.task.content + '</P>');
+          console.log(response.task.content);
+        },
+        error: function(request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+    };
+  };
+
+  $('#addBtn').on('click', addTask);
 });
 
 
