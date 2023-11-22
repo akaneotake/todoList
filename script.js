450e9e2516d9060c6401efd4686ac3385984be03
Akane <akane1033@gmail.com>
2023-11-22 21:40:44 +0100

    Changing task status(completed/active) by checkbox

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
   // GET tasks
-  $.ajax({
-    type: 'GET',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1090',
-    dataType: 'json',
-    success: function(response, textStatus) {
-      response.tasks.forEach(function(task) {
-        $('#list').append('<p>' + task.content + '</P>');
-      });
-    },
-    error: function(request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
-  });
+  var displayTasks = function() {
+    $.ajax({
+      type: 'GET',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1090',
+      dataType: 'json',
+      success: function(response, textStatus) {
+        var insertInput = function(status) {
+          if (status == true) {
+            return '<input class="checkbox completed" type="checkbox" checked/>';
+          } else {
+            return '<input class="checkbox active" type="checkbox" />';
+          };
+        };
+        response.tasks.forEach(function(task) {
+          $('#list').append('<p data-id=' + task.id + '>' + insertInput(task.completed) + task.content + '<button class="deleteBtn">DELETE</button></P>');
+        });
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
 
-  // POST tasks
+  // Create tasks (POST)
   var addTask = function() {
-    var newTask = document.getElementById('newTask').value;
-    if (newTask) {
+    if ($('#newTask').val()) {
       $.ajax({
         type: 'POST',
         url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1090',
@@ -25,12 +33,12 @@ $(document).ready(function() {
         dataType: 'json',
         data: JSON.stringify({
           task: {
-            content: newTask
+            content: $('#newTask').val()
           }
         }),
         success: function(response, textStatus) {
-          $('#list').append('<p>' + response.task.content + '</P>');
-          console.log(response.task.content);
+          $('#list').append('<p data-id=' + response.task.id + '><input class="checkbox active" type="checkbox" />' + response.task.content + '<button class="deleteBtn">DELETE</button></P>');
+          $('#newTask').val("");
         },
         error: function(request, textStatus, errorMessage) {
           console.log(errorMessage);
@@ -39,7 +47,88 @@ $(document).ready(function() {
     };
   };
 
+  // DELETE tasks
+  var deleteTask = function(id) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=1090',
+      dataType: 'json',
+      success: function(response, textStatus) {
+        $('#list [data-id=' + id + ']').remove();
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+  // Mark Completed (PUT)
+  var markCompleted = function(id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=1090',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          completed: true
+        }
+      }),
+      success: function(response, textStatus) {
+        console.log(response);
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+  
+  // Return Active (PUT)
+  var returnActive = function(id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=1090',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          completed: false
+        }
+      }),
+      success: function(response, textStatus) {
+        console.log(response);
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+  
+  // Functions and Event Handlers
+  // Get and render all tasks
+  displayTasks();
+
+  // Add a new task by clicking add button
   $('#addBtn').on('click', addTask);
+
+  // Delete the task by clicking delete button
+  $(document).on('click', '.deleteBtn', function() {
+    deleteTask($(this).parent('p').data('id'));
+  });
+
+  // Change the task status completed/Active by checkbox
+  $(document).on('change', '.checkbox', function() {
+    if (this.checked) {
+      markCompleted($(this).parent('p').data('id'));
+      $(this).removeClass('active');
+      $(this).addClass('completed');
+    } else {
+      returnActive($(this).parent('p').data('id'));
+      $(this).removeClass('completed');
+      $(this).addClass('active');
+    }
+  });
+
+
 });
-
-
